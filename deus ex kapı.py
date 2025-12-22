c3193cf0e2038862435817e164accafd9406aa5f
BeastWithin <BeastWithin@users.noreply.github.com>
2025-12-22 10:25:55 +0300

    Update deus ex kapı.py

--- deus ex kapı.py
+++ deus ex kapı.py
@@ -1,66 +1,65 @@
-import network, machine, socket,time
-
-
-ssid,password = '000000','0000000'
+import network, machine, socket,time,re
+from psw import *
 
 led = machine.Pin("LED", machine.Pin.OUT, value=1)
-pin_1up = machine.Pin(20, machine.Pin.OUT, value=0)
-pin_1down = machine.Pin(17, machine.Pin.OUT, value=0)
-pin_2up = machine.Pin(10, machine.Pin.OUT, value=0)
-pin_2down = machine.Pin (14, machine.Pin.OUT, value=0)
+pin_1up = machine.Pin(20, machine.Pin.OUT, value=1)
+pin_1down = machine.Pin(17, machine.Pin.OUT, value=1)
+pin_2up = machine.Pin(10, machine.Pin.OUT, value=1)
+pin_2down = machine.Pin (14, machine.Pin.OUT, value=1)
+doorstate = ("1. door opening.","1. door closing.","2. door opening.","2. door closing.")
 
-def blink_led(frequency = 0.5, num_blinks = 3):
+def blink_led(frequency = 0.5, num_blinks = 1):
     for _ in range(num_blinks):
-        led.on()
+        led.toggle()
         time.sleep(frequency)
-        led.off()
+        led.toggle()
         time.sleep(frequency)
-
+        
 def webpage(temperature, state):
-    #Template HTML
-    html = f"""
 
-<body>
+    body=f"""
+<body style="fcw bgb ff">
 <div class="endis" >
-<div style="width:20%;border-radius:5px;background: linear-gradient(160deg, rgba(55, 56, 88, 1) 0%, rgba(165, 164, 245, 1) 100%);"> <img height="18px" width="18px"align="left" alt="DXlogo" width="40%" src=></img>Surveillance</div>
+<div style="width:20%;border-radius:5px;background: linear-gradient(160deg, #373858 0%, #a5a4f5 100%);"> 
+<img height="18px" width="18px"align="left" alt="DXlogo" width="40%" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAMAAABhEH5lAAAAbFBMVEUAAAAAAQQAAAcCAwkEBg4HCRIYGSAAAQ0TFBtmYmANDxhHR0ojJCpZVU8tLTJCQT46OkCDf353dHMOFC05Ny1dWlhxbWeOiYa3s7M1OVGemJUYHTErKCCGgXvJx8f7+/mlpKSAgYvm5+rCwsHuJdquAAAAuUlEQVQY00XPB26EMBAF0Knupne2Jve/Y8jCgqWRpSePvj8AADMh/B/E/QYUJx9jEf7S7abEyKJ6UtF1jsQGx8ciSVfGbKxJ9hAgjcPg+1TKVwBddP298YYuMhVRUdfFReSMFbuMjdJJKgJ+0rrC8xmzwONtZ38mbobpGdt2C7gImp95WmN5BTD8TnZu12yP7yNTeD6qYm1z2HsjkaTXa6l92ae9N2k5LuO9GXzORwFU45zZJgT9tPwDgA8IM61mBcIAAAAASUVORK5CYII=">Surveillance</div>
 <div style="width:600px;background-color:#615f62">
-Active Window Options <img align="right" alt="DXlogo" width="22%" ></img>
+Active Window Options <img src="mj13logo.svg" align="right" alt="DXlogo" width="22%" >
 <div>
- <form action="./dr1u"><input class="button1" type="submit" name="door1" value="Up"><input placeholder="{state}" type="text"/></form>
-<br>
- <form action="./dr1d"><input class="button1" type="submit" name="door1" value="Down"><input placeholder="{state}" type="text"/></form>
-<br>
- <form action="./dr2u"><input class="button1" type="submit" name="door2" value="Up"><input placeholder="{state}" type="text"/></form>
-<br>
- <form action="./dr2d"><input class="button1" type="submit" name="door2" value="Down"><input placeholder="{state}" type="text"/></form>
+ <form action="./control">
+  <input class="bt ff" type="submit" name="door1" value="Up"><input placeholder="{state}" type="text"/><br>
+  <input class="bt ff" type="submit" name="door1" value="Down"><input placeholder="{state}" type="text"/><br>
+  <input class="bt ff" type="submit" name="door2" value="Up"><input placeholder="{state}" type="text"/><br>
+  <input class="bt ff" type="submit" name="door2" value="Down"><input placeholder="{state}" type="text"/>
 </div>
-<div class="cams">
-<div >cam1</div>
-<div >cam2</div>
-<div >cam3</div>
-</div>
-<div class="mf">
-Temperature is {temperature}
-</div>
+ <div class="cams">
+  <div>cam1</div><div>cam2</div><div>cam3</div>
+ </div>
+ <div class="mf">Temperature is {temperature}</div>
 </div>
 <div style="transform: translate(90%, 90%);"><input type="submit" value="Disconnect" style="align:right;width:auto"></div>
 </div>
 </body>
-</html>"""
-    html2= """           <!DOCTYPE html>
+</html>
+"""
+    head="""
+<!DOCTYPE html>
 <html>
-<head></head>
+<head>
 <style>
-.endis		{width:600px}
-.cams div	{border-radius:0px;width:33%; float:left; height:144px;border-width:1px;border-color:black;border-style:solid; background-image: ul()}
-.cams		{width:100%; overflow:auto;}
-body		{color:white; background-color: black;font-family:Arial, Helvetica, sans-serif; }
-input		{color:white;background-color:#6c6b69; border-color:white; width: 100px;  clip-path: polygon(0 5px,11px 0,100% 0,100% calc(100% - 5px),calc(100% - 11px) 100%,0 100%); border-width:2px}
-.button1		{font-family:Arial, Helvetica, sans-serif;width: 160px; text-align:left; color:white; background-color: #d4d2d4; clip-path: polygon(0 5px,11px 0,100% 0,100% calc(100% - 5px),calc(100% - 11px) 100%,0 100%);}
-div			{border-radius: 3px 3px 3px 9px;}
-.mf			{text-align:center;background-color:#4b4b4d;position:middle;width:90%;border-width:1px;border-color:black;border-style:solid;border-radius:8px 0px 0px 0px}
-</style>"""
-    return str(html2)+str(html)
+.endis {width:1000px}
+.cams div {float:left; border: solid 1px #000;border-radius:0px;width: 33%;height: 144px;margin: auto;background-image: repeating-radial-gradient(circle at 17% 32%, white, black 0.00085px);animation: back 5s linear infinite;}
+.cams {width:100%; overflow:auto;}
+input {color:white;background-color:#6c6b69; border: solid 2px #fff; width: 100px;  clip-path: polygon(0 5px,11px 0,100% 0,100% calc(100% - 5px),calc(100% - 11px) 100%,0 100%);}
+.bt {font-family:Arial, Helvetica, sans-serif;width: 160px; text-align:left; color:white; background-color: #d4d2d4; clip-path: polygon(0 5px,11px 0,100% 0,100% calc(100% - 5px),calc(100% - 11px) 100%,0 100%);}
+div {border-radius: 3px 3px 3px 9px;}
+.mf {text-align:center;background-color:#4b4b4d;position:middle;width:90%;border-width:1px;border-color:black;border-style:solid;border-radius:8px 0px 0px 0px}
+.bgw {background-color:#fff;} .bgb {background-color:#000;} .fcw {color:#fff;} .fcb {color:#000;} .ff {font-family:Arial, Helvetica, sans-serif; }
+@keyframes back {from {background-size: 100% 100%;}to{background-size: 200% 200%;}
+}
+</style></head>
+"""
+    html=head+body
+    return html#temp.format(temperature,state)
 
 def connect():
     #Connect to WLAN
@@ -87,31 +86,34 @@ def open_socket(ip):
 def serve(connection):
     #Start a web server
     state = 'OFF'
-    led.off()
+    led.toggle()
     temperature = 0
     while True:
         client = connection.accept()[0]
         request = client.recv(1024)
         request = str(request)
+        #print("**İstem** "+request)
         try:
-            print(request)
-            request = request.split()[1]
-        except IndexError:
+            #print(request)
+            query = request.split()[1]
+            print("**Sorgu** ",query)
+        except IndexError as I:
+            print(I)
             pass
-        if request == '/dr1u?door1=Up':
-            state = "1. kapı açılıyor."
+        if query == '/control?door1=Up':
+            state = doorstate[0] #1. door opening.
             print(state)
             control_door(state)
-        elif request =='/dr1d?door1=Down':
-            state = "1. kapı kapanıyor."
+        elif query =='/control?door1=Down':
+            state = doorstate[1] #1. door closing.
             print(state)
             control_door(state)
-        elif request =='/dr2u?door2=Up':
-            state = "2. kapı açılıyor."
+        elif query =='/control?door2=Up':
+            state = doorstate[2] #"2. door opening."
             print(state)
             control_door(state)
-        elif request =='/dr2d?door2=Down':
-            state = "2. kapı kapanıyor."
+        elif query =='/control?door2=Down':
+            state = doorstate[3] #"2. door closing."
             print(state)
             control_door(state)
         temperature = onboard_temp()
@@ -121,25 +123,25 @@ def serve(connection):
 
 
 def control_door(cmd):
-    if cmd == "1. kapı açılıyor.":
-        pin_1up.on()
-        blink_led(0.1, 1)
-        pin_1up.off()
+    if cmd == doorstate[0]: # 1. Door Opening
+        pin_1up.toggle()
+        time.sleep(0.5)
+        pin_1up.toggle()
         
-    if cmd == "1. kapı kapanıyor.":
-        pin_1down.on()
-        blink_led(0.1, 1)
-        pin_1down.off()
+    if cmd == doorstate[1]: # 1. Door Closeing
+        pin_1down.toggle()
+        time.sleep(0.5)
+        pin_1down.toggle()
 
-    if cmd == "2. kapı açılıyor.":
-        pin_2up.on()
-        blink_led(0.1, 1)
-        pin_2up.off()
+    if cmd == doorstate[2]: # 2. Door Opening
+        pin_2up.toggle()
+        time.sleep(0.5)
+        pin_2up.toggle()
 
-    if cmd == "2. kapı kapanıyor.":
-        pin_2down.on()
-        blink_led(0.1, 1)
-        pin_2down.off()
+    if cmd == doorstate[3]: # 2. Door Closing
+        pin_2down.toggle()
+        time.sleep(0.5)
+        pin_2down.toggle()
 
 def onboard_temp():
     adc = machine.ADC(4)
@@ -149,12 +151,12 @@ def onboard_temp():
 
 try:
     ip = connect()
+    print("IP: ",ip)
     connection = open_socket(ip)
+    print("Connection: ",connection)
     serve(connection)
 except Exception as e:
     print(e)
-    connection.close()
-finally:
-    connection.close()
-
+    time.sleep(1)
+    machine.reset()
 
